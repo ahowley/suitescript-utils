04c6bdadd4ed935fa86083891f47d1b3b01d280f
Alex Howley <ahowley@nobilisgroup.com>
2024-05-10 13:25:21 -0500

    CBW-674 finish creating options for adding new fields to serverWidget forms in ui module

--- src/TypeScripts/netsuite-ui.ts
+++ src/TypeScripts/netsuite-ui.ts
@@ -5,7 +5,7 @@
  * This module contains functionality for building NetSuite user interfaces using N/ui.
  */
 
-import record from "N/record";
+import record, { Field, Type as RecordType } from "N/record";
 import serverWidget, { FieldType } from "N/ui/serverWidget";
 import { throwError } from "./errors";
 
@@ -37,14 +37,47 @@ type UniversalFieldOptions = {
 type TextFieldOptions = UniversalFieldOptions & {
   type: FieldType.TEXT | FieldType.TEXTAREA | FieldType.LONGTEXT;
 };
+type HtmlFieldOptions = UniversalFieldOptions & {
+  type: FieldType.INLINEHTML;
+};
+type EmailFieldOptions = UniversalFieldOptions & {
+  type: FieldType.EMAIL;
+};
+type PasswordFieldOptions = UniversalFieldOptions & {
+  type: FieldType.PASSWORD;
+};
+type PhoneFieldOptions = UniversalFieldOptions & {
+  type: FieldType.PHONE;
+};
 type IntegerFieldOptions = UniversalFieldOptions & {
-  type: FieldType.INTEGER;
+  type: FieldType.INTEGER | FieldType.PERCENT;
 };
 type DecimalFieldOptions = UniversalFieldOptions & {
   type: FieldType.FLOAT | FieldType.CURRENCY;
 };
+type DateFieldOptions = UniversalFieldOptions & {
+  type: FieldType.DATE | FieldType.DATETIMETZ;
+};
+type SelectFieldOptions = UniversalFieldOptions & {
+  type: FieldType.SELECT | FieldType.MULTISELECT;
+  source: RecordType;
+};
+type CustomSelectFieldOptions = UniversalFieldOptions & {
+  type: FieldType.SELECT | FieldType.MULTISELECT;
+};
 
-type FieldOptions = UniversalFieldOptions | TextFieldOptions | IntegerFieldOptions | DecimalFieldOptions;
+type FieldOptions =
+  | UniversalFieldOptions
+  | TextFieldOptions
+  | HtmlFieldOptions
+  | EmailFieldOptions
+  | PasswordFieldOptions
+  | PhoneFieldOptions
+  | IntegerFieldOptions
+  | DecimalFieldOptions
+  | DateFieldOptions
+  | SelectFieldOptions
+  | CustomSelectFieldOptions;
 /**
  * Generate  field properties for a field that will go in a custom form, sublist, tab, or field group.
  *
@@ -75,11 +108,43 @@ export const field: { [fieldType: string]: FieldFunction } = {
 
     return { id, label, type: fieldType() };
   },
+  html: (id: FieldId, label: string): HtmlFieldOptions => {
+    return {
+      id,
+      label,
+      type: FieldType.INLINEHTML as FieldType.INLINEHTML,
+    };
+  },
+  email: (id: FieldId, label: string): EmailFieldOptions => {
+    return {
+      id,
+      label,
+      type: FieldType.EMAIL as FieldType.EMAIL,
+    };
+  },
+  password: (id: FieldId, label: string): PasswordFieldOptions => {
+    return {
+      id,
+      label,
+      type: FieldType.PASSWORD as FieldType.PASSWORD,
+    };
+  },
+  phone: (id: FieldId, label: string): PhoneFieldOptions => {
+    return {
+      id,
+      label,
+      type: FieldType.PHONE as FieldType.PHONE,
+    };
+  },
   /**
-   * @param label - The human-readable field name that will appear in the UI.
+   * @param isPercent - Treat this integer as a percentage between 0 and 100.
    */
-  int: (id: FieldId, label: string): IntegerFieldOptions => {
-    return { id, label, type: FieldType.INTEGER as FieldType.INTEGER };
+  int: (id: FieldId, label: string, isPercent: boolean): IntegerFieldOptions => {
+    return {
+      id,
+      label,
+      type: isPercent ? (FieldType.PERCENT as FieldType.PERCENT) : (FieldType.INTEGER as FieldType.INTEGER),
+    };
   },
   /**
    * @param isCurrency - Whether the numbers in this field should be considered currency.
@@ -89,6 +154,42 @@ export const field: { [fieldType: string]: FieldFunction } = {
       id,
       label,
       type: isCurrency ? (FieldType.CURRENCY as FieldType.CURRENCY) : (FieldType.FLOAT as FieldType.FLOAT),
+    };
+  },
+  /**
+   * @param dateOnly - Whether to exclude the time component from this field, only storing the date.
+   */
+  date: (id: FieldId, label: string, dateOnly: boolean): DateFieldOptions => {
+    return {
+      id,
+      label,
+      type: dateOnly ? (FieldType.DATE as FieldType.DATE) : (FieldType.DATETIMETZ as FieldType.DATETIMETZ),
+    };
+  },
+  /**
+   * @param recordType - the type of record to select from in this select field. For a custom select field,
+   * use field.customSelect() instead.
+   * @param multiselect - whether to allow multiple selections from the options at once.
+   */
+  select: (id: FieldId, label: string, recordType: RecordType, multiselect: boolean): SelectFieldOptions => {
+    return {
+      id,
+      label,
+      type: multiselect ? (FieldType.MULTISELECT as FieldType.MULTISELECT) : (FieldType.SELECT as FieldType.SELECT),
+      source: recordType,
+    };
+  },
+  /**
+   * Create options for a select field with custom entries to select from. Note: options must be added with
+   * Field.addSelectOption(options)
+   *
+   * @param multiselect - whether to allow multiple selections from the options at once.
+   */
+  customSelect: (id: FieldId, label: string, multiselect: boolean): CustomSelectFieldOptions => {
+    return {
+      id,
+      label,
+      type: multiselect ? (FieldType.MULTISELECT as FieldType.MULTISELECT) : (FieldType.SELECT as FieldType.SELECT),
     };
   },
 };
