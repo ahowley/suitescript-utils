334fedc0de2abf3cca0765d88abba6b630278e74
Alex Howley <ahowley@nobilisgroup.com>
2024-05-13 15:28:09 -0500

    CBW-674 remove compiled javascript from repo

--- src/FileCabinet/SuiteScripts/utils/common.js
+++
@@ -1,9 +0,0 @@
-/**
- * common.ts
- * @NApiVersion 2.1
- * A collection of common types, basic utility functions, etc. used across SDF account customization projects.
- */
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-});
