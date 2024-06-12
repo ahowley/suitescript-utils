7869850c0e6ed4728991360958bdad190424560a
Alex Howley <ahowley@nobilisgroup.com>
2024-06-12 13:18:18 -0500

    CBW-704 add logging decorator function

---
+++ src/TypeScripts/logging.ts
@@ -0,0 +1,34 @@
+/** logging.ts
+ * @NApiVersion 2.1
+ *
+ * This module contains some "decorator"-style functions for logging execution in a SuiteScript.
+ */
+
+import log from "N/log";
+import { remainingUsage } from "./environment";
+
+/**
+ * Wrap a function definition with this decorator to log its execution when the script it's called from is deployed
+ * in "debug" mode.
+ *
+ * @param func - The function being defined.
+ * @param logTitlePrefix - Add this to show a message before the auto-generated log title.
+ */
+export function debugLog<Func extends (...args: any[]) => any>(
+  func: Func,
+  logTitlePrefix?: string,
+): (...args: Parameters<Func>) => ReturnType<Func> {
+  return (...args: Parameters<Func>) => {
+    log.debug(
+      `${logTitlePrefix ? logTitlePrefix + " - " : ""}${func.name} invoked`,
+      `Calling function ${func.name}. Remaining usage: ${remainingUsage()}`,
+    );
+    const result: ReturnType<Func> = func(...args);
+    log.debug(
+      `${logTitlePrefix ? logTitlePrefix + " - " : ""}${func.name} returned`,
+      `Returning result from ${func.name}. Remaining usage: ${remainingUsage()}`,
+    );
+
+    return result;
+  };
+}
